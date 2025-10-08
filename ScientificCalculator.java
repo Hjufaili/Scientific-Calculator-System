61f38ced25c69dae78ff48073b8ac660e5f9184e
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-08 11:42:36 +0400

    Add (add function)

---
+++ ScientificCalculator.java
@@ -0,0 +1,14 @@
+public class ScientificCalculator {
+
+    public static double lastResult;
+
+    public static double add(double a, double b) {
+        lastResult = a + b;
+        return lastResult;
+
+    }
+
+    
+
+}
+
