3bb4107b904700cbb505e77a1837a90ebffa9d10
Alharith Aljufaili <gakhanaljufaily@gmail.com>
2025-10-08 11:44:27 +0400

    Add (multiply function)

--- ScientificCalculator.java
+++ ScientificCalculator.java
@@ -5,13 +5,17 @@ public class ScientificCalculator {
     public static double add(double a, double b) {
         lastResult = a + b;
         return lastResult;
-
     }
+
     public static double subtract(double a, double b) {
         lastResult = a - b;
         return lastResult;
     }
 
+    public static double multiply(double a, double b) {
+        lastResult = a * b;
+        return lastResult;
+    }
 
 
 }
